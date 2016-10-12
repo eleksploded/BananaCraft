844880b680ce50c0f58a615f46ea78e9f554b869
pikachu923 <pikachu9234@gmail.com>
2016-10-11 18:22:25 -0700

    Update index.php

--- docs/index.php
+++ docs/index.php
@@ -8,7 +8,7 @@
 <body>
 
 <div id="container">
-	<?php include(header.php)?>
+	<?php include("header.php")?>
 		<div id="main">
 			<p>Hello,<br/>&nbsp; Thanks for caring about my mod! Bananacraft adds bananas, banana related items, and non-banana related Items.</p>
 			<a href="http://patreon.com/pikachu923" rel="nofollow">&nbsp;<img src="http://i.imgur.com/LGIXuQE.png" alt="" width="152" height="38"></a>
