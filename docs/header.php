f63df13fdea04941ea3926e3acf1b13704807fea
pikachu923 <pikachu9234@gmail.com>
2016-10-11 18:20:41 -0700

    Create header.php

---
+++ docs/header.php
@@ -0,0 +1,13 @@
+<div id="header">
+	<h1>BananaCraft</h1>
+	</div>
+	<div id="content">
+		<div id="nav">
+		<h3>Navigation</h3>
+			<ul>
+				<li><a href="index.html"><b>Home</b></a></li>
+				<li></li>
+				<li></li>
+				<li><a href="contact.html"><b>Contact</b></a></li>
+			</ul>
+		</div>
