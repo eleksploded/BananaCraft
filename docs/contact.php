8293e178fa1fc956e87b4bf7775f4e6263385f6d
pikachu923 <pikachu9234@gmail.com>
2016-10-12 07:59:36 -0700

    Update contact.php

--- docs/contact.php
+++ docs/contact.php
@@ -6,7 +6,19 @@
 <body>
 
 <div id="container">
-	<?php include("header.php") ?>
+	<div id="header">
+	<h1>BananaCraft</h1>
+	</div>
+	<div id="content">
+		<div id="nav">
+		<h3>Navigation</h3>
+			<ul>
+				<li><a href="index.html"><b>Home</b></a></li>
+				<li></li>
+				<li></li>
+				<li><a href="contact.html"><b>Contact</b></a></li>
+			</ul>
+		</div>
 		<ul>
 			<p>The BananaCraft team; </p>
 			<li>&nbsp; Pikachu923: Auther: pikachu9234@gmail.com </li>
